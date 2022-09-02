7da96838369066a0b1927a63c29be6e05ca8c375
Tehmoor Amjad <AR.Developer@accessretailpk.local>
2022-09-02 11:23:18 +0500

    Working on Register

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from "./pages/Home";
 import ProductList from "./pages/ProductList";
+import Product from "./pages/Product";
+import Register from "./pages/Register";
 
 const App = () => {
   return(
       <>
-      <ProductList/>
+      <Register/>
       </>
   )
 }
---
+++ src/pages/Login.js
@@ -0,0 +1,16 @@
+import styled from "styled-components"
+
+const Container = styled.div`
+
+`;
+
+const Login = () => {
+    return(
+        <Container>
+
+        </Container>
+
+    )
+}
+
+export default Login;
